433237e2656b8504171ada7c91114e3acf782189
David Pich <davidpich.kh@gmail.com>
2024-03-26 19:21:34 +0900

    style: updat daisyUI version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,19 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e3b35b",
+          secondary: "#e3b35b",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
+  },
 };
